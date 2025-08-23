be5a659c1ec1e1d998a7e9fcf1e68d2696d2eca6
Farmerintech <farmerintech@gmail.com>
2025-08-23 20:10:06 +0100

    Receipe

--- src/components/Receipe.jsx
+++ src/components/Receipe.jsx
@@ -17,30 +17,35 @@ export const Receipe = () =>{
             desc:'Big and Juicy Wagyu Beef Cheeseburger',
             image:Receipe1
         },
-         {
+        {
             desc:'Big and Juicy Wagyu Beef Cheeseburger',
             image:Receipe2
         },
-         {
+        {
             desc:'Big and Juicy Wagyu Beef Cheeseburger',
             image:Receipe3
         },
-         {
+        {
             desc:'Big and Juicy Wagyu Beef Cheeseburger',
             image:Receipe4
-        },  {
+        },  
+        {
             desc:'Big and Juicy Wagyu Beef Cheeseburger',
             image:Receipe5
-        },  {
+        },  
+        {
             desc:'Big and Juicy Wagyu Beef Cheeseburger',
             image:Receipe6
-        },  {
+        },  
+        {
             desc:'Big and Juicy Wagyu Beef Cheeseburger',
             image:Receipe7
-        },  {
+        },  
+        {
             desc:'Big and Juicy Wagyu Beef Cheeseburger',
             image:Receipe8
-        },  {
+        },  
+        {
             desc:'Big and Juicy Wagyu Beef Cheeseburger',
             image:Receipe9
         },
@@ -53,25 +58,27 @@ export const Receipe = () =>{
                 <p>Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor 
                     incididunt ut labore et dolore magna aliqut enim ad minim </p>
             </div>
-            <div className="flex flex-wrap gap-10 justify-center md:justify-between items-center">
+            <div className="flex flex-wrap gap-2 md:gap-10 justify-between items-center">
                 {
                     Receipes.map(item=>(
                         <div 
                         style={{
                            background: "linear-gradient(180deg, rgba(231, 249, 253, 0.00) 0%, #E7F9FD 100%)",
                          }}
-                        className="w-[250px] h-full rounded-[30px] px-2 py-5">
+                        className="w-[170px] md:w-[250px] h-full rounded-2xl md:rounded-[30px] px-3 md:px-3 py-5">
                             <div className="">
-                                <img src={item.image} className="w-[350px] rounded-[15px] h-[158px]"/>
+                                <img src={item.image} className="w-[150px] md:w-[350px] rounded-3xl h-[158px]"/>
                                 <div className="absolute top-4 left-5"><FaHeart/></div>
                             </div>
                             <p className="font-[600] mb-2">{item.desc}</p>
-                            <div className="flex gap-5">
-                                <p className="flex gap-2 items-center justify-center">
-                                    <span><FaClock size={10}/></span> <span>30 Minutes</span>
+                            <div className="md:flex gap-5">
+                                <p className="flex gap-2 items-center md:justify-center">
+                                    <span><FaClock size={10}/></span> 
+                                    <span>30 Minutes</span>
                                 </p>
-                                 <p className="flex gap-2 items-center justify-center">
-                                    <span><FaSpoon size={10}/></span> <span>Snacks</span>
+                                <p className="flex gap-2 items-center md:justify-center">
+                                    <span><FaSpoon size={10}/></span> 
+                                    <span>Snacks</span>
                                 </p>
                             </div>
                         </div>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,7 +4,7 @@ import { Receipe } from "../components/Receipe";
 
 export default function Home(){
 return(
-    <section className="md:px-[64px] px-[16px] pb-20 flex flex-col gap-[32px]">
+    <section className="md:px-[64px] px-[16px] pb-20 pt-10 flex flex-col gap-[100px]">
         <Hero/>    
         <Categories/>
         <Receipe/>
